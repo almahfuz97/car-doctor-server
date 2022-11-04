b85249bc3381f57a8dbbe9d008a00d852084ad80
almahfuz <almahfuz0327@gmail.com>
2022-11-04 10:14:01 +0600

    new apis

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -20,8 +20,47 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('car-doctor').collection('services');
+        const orderCollection = client.db('car-doctor').collection('orders');
 
+        app.get('/', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            console.log(result)
+            res.send(result);
+        })
+
+        // orders
+        app.get('/orders', async (req, res) => {
+            const queryEmail = req.query.email;
+
+            const query = {
+                email: queryEmail
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+        // delete
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            }
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
     } catch (error) {
 
     }
